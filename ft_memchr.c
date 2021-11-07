0a1df4bac12d0e849b0d1f3cb4fcfd9acb2fc884
Imane El mohalil <iel-moha@e2r4p9.1337.ma>
2021-11-07 21:48:23 +0100

    cm

---
+++ ft_memchr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iel-moha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/07 21:02:56 by iel-moha          #+#    #+#             */
+/*   Updated: 2021/11/07 21:31:41 by iel-moha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char *p;
+
+	p = (unsigned char *)s;
+	while(p && n > 0)
+	{
+		if(*p == (unsigned char)c)
+			return (p);
+		p++;
+		n--;
+	}
+	return NULL;
+}
+/*int main()
+{
+	char *s = "imanae";
+	int c = 'a';
+
+	printf("%s\n",ft_memchr(s,c,1));
+	printf("%s\n", memchr(s,c,1));
+}*/
--- libft.h
+++ libft.h
@@ -23,5 +23,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
+void *memchr(const void *s, int c, size_t n);
+
 
 #endif
