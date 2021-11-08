af449a85ba839236f599a7f4ec5b9245beac16dc
Imane El mohalil <elmohalilimane@gmail.com>
2021-11-08 20:50:16 +0100

    strnstr 1

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,12 +6,14 @@
 /*   By: iel-moha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 15:02:40 by iel-moha          #+#    #+#             */
-/*   Updated: 2021/11/08 15:10:02 by iel-moha         ###   ########.fr       */
+/*   Updated: 2021/11/08 20:49:45 by iel-moha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 char	*strnstr(const char *haystack, const char *needle, size_t len)
 {
-	
+	if(needle == NULL)
+		return (haystack);
+
 }
