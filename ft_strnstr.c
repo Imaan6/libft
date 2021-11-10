e69246ecb288b636ba0ae3bdcecfdd908d42032d
Imane El mohalil <elmohalilimane@gmail.com>
2021-11-10 23:00:19 +0100

    fixed strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,29 +6,46 @@
 /*   By: iel-moha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 15:02:40 by iel-moha          #+#    #+#             */
-/*   Updated: 2021/11/09 12:32:09 by iel-moha         ###   ########.fr       */
+/*   Updated: 2021/11/10 22:53:37 by iel-moha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
 	size_t	j;
 
-	if(needle == NULL)
-		return (char*)(haystack);
 	i = 0;
+	if (!ft_strncmp(haystack, "", ft_strlen(haystack))
+		&& !ft_strncmp(needle, "", ft_strlen(needle)))
+			return ("");
+	if(haystack == NULL)
+	{
+		return (char*)(haystack);
+	}
 	while (haystack[i] && i < len)
 	{
 		j = 0;
-		while (haystack[i + j] != '\0' && haystack[i + j] == needle[j] && i + j < len )
+		while (haystack[i + j] != '\0' && haystack[i + j] == needle[j] && i + j < len)
 		{
 			if (needle[j + 1] == '\0')
 				return (char*)(&haystack[i]);
-			j++;
+		j++;
 		}
 		i++;
 	}
-	return (0);
+	return (NULL);
 }
+
+/*
+int main()
+{
+	char *haystack = "";
+	char *needle = "";
+
+	printf("%s\n",ft_strnstr(haystack, needle, 0));
+	printf("%s\n",strnstr(haystack, needle, 0));
+}
+*/
