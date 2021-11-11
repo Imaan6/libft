98c9dbc8330aa41c3537f08873c77423d7e9986e
Imane El mohalil <elmohalilimane@gmail.com>
2021-11-11 15:27:10 +0100

    rap part1

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: iel-moha <iel-moha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 13:11:54 by iel-moha          #+#    #+#             */
-/*   Updated: 2021/11/07 12:28:42 by iel-moha         ###   ########.fr       */
+/*   Updated: 2021/11/11 15:19:26 by iel-moha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,27 @@
 size_t	ft_strlcat(char *  dst, const char *  src, size_t fdstsize)
 {
 	size_t	i;
-	size_t	src_size;
-	size_t	dst_size;
+	size_t	src_len;
+	size_t	dst_len;
 
-	src_size = ft_strlen(src);
-	dst_size = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	dst_len = ft_strlen(dst);
 	if (fdstsize == 0)
 	{
-		return (src_size);
+		return (src_len);
 	}
 	i = 0;
-	while (src[i] != '\0' && i + dst_size < (fdstsize - 1))
+	while (src[i] != '\0' && i + dst_len < (fdstsize - 1))
 	{
-		dst[i + dst_size] = src[i];
+		dst[i + dst_len] = src[i];
 		i++;
 	}
-	dst[i + dst_size] = '\0';
-	return (src_size + fdstsize);
-}/*
+	dst[i + dst_len] = '\0';
+	if(dst_len < fdstsize)
+		return(src_len + dst_len);
+	else
+		return (src_len + fdstsize);
+}
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -39,6 +42,6 @@ int	main()
 {
 	char txt1[60] = "imane ";
 	char txt2[] = "fiha n3ass";
-	printf ("%lu\n%s", ft_strlcat(txt1, txt2, 0), txt1);
+	printf ("%lu\n%s", ft_strlcat(txt1, txt2, 11), txt1);
 		return 0;
-}*/
+}
