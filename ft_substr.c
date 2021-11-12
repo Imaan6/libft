54afac2ab49aacf95ac27a5f568785f181ccceff
Imane El mohalil <elmohalilimane@gmail.com>
2021-11-12 23:59:38 +0100

    ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,11 +6,30 @@
 /*   By: iel-moha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 22:41:32 by iel-moha          #+#    #+#             */
-/*   Updated: 2021/11/10 22:46:38 by iel-moha         ###   ########.fr       */
+/*   Updated: 2021/11/12 23:59:07 by iel-moha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 char	*ft_substr(char const *s, unsigned int start,size_t len)
 {
-	
-}
+	char *sub_s;
+	int	i;
+
+	sub_s = (char *)malloc((len + 1) * sizeof(char));
+	if(!sub_s)
+		return (NULL);
+	i = 0;
+	while(i < len)
+	{
+		sub_s[i] = (char)s[start];
+		i++;
+		start++;
+	}
+	sub_s[i] = '\0';
+	return (sub_s);
+}/*
+int main()
+{
+	char *s = "La vie est belle";
+	printf("%s", ft_substr(s, 3, 10));
+}*/
