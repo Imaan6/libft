ea663dd35a0fa7a20e4a03e346d4552fc7310113
Imane El mohalil <elmohalilimane@gmail.com>
2021-11-14 23:15:48 +0100

    strmapi

---
+++ ft_strmapi.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iel-moha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/14 20:30:07 by iel-moha          #+#    #+#             */
+/*   Updated: 2021/11/14 23:14:57 by iel-moha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*char upper(unsigned int i, char c)
+{
+	if (i % 2 == 0)
+		c = '$';
+	return (c);
+}*/
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	int	i;
+	char	*new;
+
+	i = 0;
+	
+	new = (char *)malloc((ft_strlen(s) + 1) * sizeof(char));
+	while(s[i])
+	{
+		new[i] = (*f) (i, s[i]);
+		i++;
+	}
+	new[i] = '\0';
+	return (new);
+}
+/*
+int main()
+{
+	char *s = "imane";
+	printf("%s", ft_strmapi(s, upper));
+//	ft_strmapi(s, upper);
+
+}*/
