b7761888d44b86e7dbf1de3f9db8ef7d3a7d3e11
Imane El mohalil <elmohalilimane@gmail.com>
2021-11-16 22:41:13 +0100

    putnbr

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,10 +6,42 @@
 /*   By: iel-moha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 21:54:39 by iel-moha          #+#    #+#             */
-/*   Updated: 2021/11/16 21:55:11 by iel-moha         ###   ########.fr       */
+/*   Updated: 2021/11/16 22:40:50 by iel-moha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+void	ft_putnbr_fd(int n, int fd)
+{
+	unsigned int	nb;
 
+	if (n < 0)
+	{
+		write (fd, "-", 1);
+		nb = n * -1;
+	}
+	else
+	{
+		nb = n;
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr_fd((nb / 10), fd);
+		ft_putchar_fd((nb % 10 + 48), fd);
+	}
+	if (nb < 10)
+	{
+		ft_putchar_fd((nb + 48), fd);
+	}
+}
+/*
+int main()
+{
+	int n = 42;
+	int fd;
+
+	fd = open("sample.txt", O_WRONLY | O_APPEND);
+	ft_putnbr_fd(n, fd);
+	close(fd);
+}*/
