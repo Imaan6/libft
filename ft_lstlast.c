d1178cb96ff2134263abc5ffef8a30f91fea4130
Imane El mohalil <elmohalilimane@gmail.com>
2021-11-24 18:01:33 +0100

    listsssssss

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -1,41 +1,53 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstsize.c                                       :+:      :+:    :+:   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: iel-moha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/23 23:50:37 by iel-moha          #+#    #+#             */
-/*   Updated: 2021/11/24 14:20:06 by iel-moha         ###   ########.fr       */
+/*   Created: 2021/11/24 14:09:22 by iel-moha          #+#    #+#             */
+/*   Updated: 2021/11/24 15:33:26 by iel-moha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_lstsize(t_list *lst)
+t_list *ft_lstlast(t_list *lst)
 {
-	int counter;
 	t_list	*current;
 
-	counter = 0;
 	current = lst;
-	while(current)
+	if(current == NULL)
+		return (NULL);
+	while (current->next)
 	{
 		current = current->next;
-		counter++;
 	}
-	return (counter);
+	return (current);
 }
+/*void display(t_list *lst)
+{
+	t_list *curr = lst;
+	while (curr)
+	{
+		printf("%s-->", curr->content);
+		curr = curr->next;
+	}
+	printf("NULL");
+}*/
 /*int main()
 {
 	t_list	*head= NULL;
-	t_list *new1 = ft_lstnew("first");
-	t_list *new2 = ft_lstnew("second");
-	t_list *new3 = ft_lstnew("third");
-	t_list *new4 = ft_lstnew("fourth");
+	t_list	*new1 = ft_lstnew("first");
+	t_list	*new2 = ft_lstnew("second");
+	t_list	*new3 = ft_lstnew("third");
+	t_list	*new4 = ft_lstnew("fourth");
 	ft_lstadd_front(&head, new1);
 	ft_lstadd_front(&head, new2);
 	ft_lstadd_front(&head, new3);
 	ft_lstadd_front(&head, new4);
-	printf("%d",ft_lstsize(new4));
+//	display(new4);
+	t_list	*ptr;
+	ptr = ft_lstlast(head);
+	printf("%s", (char *)ptr->content);
 }*/
