9e2bbba4a7479c15cdd45eb718e4c33b851f747a
Imane El mohalil <elmohalilimane@gmail.com>
2021-11-27 07:37:04 +0100

    iter ll

---
+++ ft_lstiter.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iel-moha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/27 06:45:17 by iel-moha          #+#    #+#             */
+/*   Updated: 2021/11/27 07:36:24 by iel-moha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*void	first_char(void *data)
+{
+	((char *)data)[0] = 'z';
+}*/
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	if(lst == NULL)
+		return ;
+	while(lst)
+	{
+		(*f)(lst->content);
+		lst = lst->next;
+	}
+}
+/*
+void display(t_list *lst)
+{
+	t_list *curr = lst;
+	while (curr)
+	{
+		printf("%s-->", curr->content);
+		curr = curr->next;
+	}
+	printf("NULL");
+}
+int main()
+{
+	t_list	*head= NULL;
+	t_list	*new1 = ft_lstnew(ft_strdup("first"));
+	t_list	*new2 = ft_lstnew(ft_strdup("second"));
+	t_list	*new3 = ft_lstnew(ft_strdup("third"));
+	t_list	*new4 = ft_lstnew(ft_strdup("fourth"));
+	ft_lstadd_front(&head, new1);
+	ft_lstadd_front(&head, new2);
+	ft_lstadd_front(&head, new3);
+	ft_lstadd_front(&head, new4);
+	display(head);
+	ft_lstiter(head, first_char);
+	display(head);
+}*/
